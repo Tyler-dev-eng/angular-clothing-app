975f105150e95a9be1b2435c77ced843f4c490f8
Tyler-dev-eng <tbrecht17@gmail.com>
2024-07-01 21:35:32 +0200

    Making the product component

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../../types';
+import { RatingModule } from 'primeng/rating';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [RatingModule, CommonModule, FormsModule],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.scss',
+})
+export class ProductComponent {
+  @Input() product!: Product;
+  @Output() productOutput: EventEmitter<Product> = new EventEmitter<Product>();
+
+  ngOnInit() {
+    this.productOutput.emit(this.product);
+  }
+}
